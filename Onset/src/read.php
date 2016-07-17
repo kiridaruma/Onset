eac8e4609e09f914e634dc5e8430d8db63ca6f95
AkagiCrafter <akg_crafter@yahoo.co.jp>
2016-07-17 11:28:44 +0900

    Refactoring r/w

--- Onset/src/read.php
+++ Onset/src/read.php
@@ -1,34 +1,33 @@
 <?php
-
+require_once('core.php');
 require_once('config.php');
 
 session_start();
 
-$time = isset($_POST['time']) && $_POST['time'] != NULL ? $_POST['time'] : FALSE;
-$room = isset($_SESSION['onset_room']) && $_SESSION['onset_room'] != NULL ? $_SESSION['onset_room'] : FALSE;
+$time = isset($_POST['time'])          && $_POST['time']          != NULL ? $_POST['time']          : false;
+$room = isset($_SESSION['onset_room']) && $_SESSION['onset_room'] != NULL ? $_SESSION['onset_room'] : false;
 
-if(!$time || !$room){
-    echo Onset::errorJson("不正なアクセス");
+if (!$time || !$room) {
+    echo Onset::jsonStatus("不正なアクセス", -1);
     die();
 }
 
+$_dir = $config['roomSavepath'].$room;
 
-$dir = $config['roomSavepath'].$room;
-
-if($time < filemtime($dir."/xxlogxx.txt") * 1000) {
+if ($time < filemtime($_dir."/xxlogxx.txt") * 1000) {
     $fp = fopen($dir."/xxlogxx.txt", 'r');
-    do{
+
+    do {
         $line = fgets($fp);
-        if($line !== FALSE) {
-            echo $line;
-        }
-    }while($line !== FALSE);
+        if($line !== false) echo $line;
+    } while($line !== false);
+
     fclose($fp);
 } else {
     echo "none";
 }
 
-$tmp = $dir."/connect/".$_SESSION['onset_id'];
+$tmp = $_dir."/connect/".$_SESSION['onset_id'];
 file_put_contents($tmp, time()."\n".$_SESSION['onset_nick'], LOCK_EX);
 
 clearstatcache();
