348ca94b47d30bb4cb170584666718ca6e6820d7
kiridaruma <twitter@kiridaruma>
2016-10-21 18:54:42 +0900

    fix checkLoginUser.php
    avoid undefined var access to suppress PHP notice log

--- Onset/public_html/src/checkLoginUser.php
+++ Onset/public_html/src/checkLoginUser.php
@@ -6,6 +6,7 @@ session_start();
 
 $room = Onset::varidate($_SESSION['onset_room']);
 $id = Onset::varidate($_SESSION['onset_id']);
+$lock = Onset::varidate($_POST['lock']);
 
 if(!$room || !$id){
     echo "不正なアクセス";
@@ -15,17 +16,23 @@ if(!$room || !$id){
 $dir = RoomSavepath.$room."/connect/";
 $arr = scandir($dir);
 
-if($_POST['lock'] === 'unlock') {
+if($lock === 'unlock') {
     file_put_contents($dir.$id, time()."\n".$_SESSION['onset_nick']);
     die();
 }
 
 file_put_contents($dir.$id, time()."\n".$_SESSION['onset_nick']."\nlocked");
 
+$ret = "";
+$num = 0;
+
 foreach($arr as $value) {
     if($value == "." || $value == "..") continue;
 
-    list($time, $nick, $isLock) = explode("\n", file_get_contents($dir.$value));
+    $arr = explode("\n", file_get_contents($dir.$value));
+    $time = $arr[0];
+    $nick = $arr[1];
+    $isLock = count($arr) > 2 ? $arr : '';
 
     if($time + 5 < time() && $isLock !== 'locked') {
         unlink($dir.$value);
