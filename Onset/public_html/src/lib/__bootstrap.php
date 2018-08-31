1096e3aa3cf7e299e98be8b6e18aa4d3786f26f2
kiridaruma <www2480www@gmail.com>
2018-08-31 13:09:13 +0900

    エラーハンドリングを削除(エラーに対してRuntimeExceptionはおかしい気がする)

--- Onset/public_html/src/lib/__bootstrap.php
+++ Onset/public_html/src/lib/__bootstrap.php
@@ -4,9 +4,3 @@
 spl_autoload_register(function ($className) {
     require __DIR__ . "/{$className}.php";
 });
-
-// エラーハンドリング(例外を投げたり、ロギングしたり)
-set_error_handler(function ($errno, $errstr, $errfile, $errline, $_) {
-    $str = sprintf("エラー[%d]:\t%s\t%s(line%d)", $errno, $errstr, $errfile, $errline);
-    throw new \RuntimeException($str);
-});
