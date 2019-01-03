7aab7988de12d5128f0585243186e0564cd38be8
kiridaruma <www2480www@gmail.com>
2019-01-03 22:08:09 +0900

    Repositoryクラスのインターフェースを追加

---
+++ src/lib/Repositories/RoomRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Onset\Repositories;
+
+use Onset\Models\Room;
+
+abstract class RoomRepository
+{
+    abstract public function get(string $id): Room;
+    abstract public function save(Room $room): void;
+    abstract public function delete(Room $room): void;
+    abstract public function gc(int $time): void;
+
+    protected function build(array $data): Room
+    {
+        $room = new Room($data['id']);
+        $room->chatlog = $data['chatlog'];
+        $room->topic = $data['topic'];
+        $room->memos = $data['memos'];
+        $room->users = $data['users'];
+        return $room;
+    }
+}
